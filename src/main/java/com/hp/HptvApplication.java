772a2d55323aeb9e81d4baf8d26f2cd9e54481ac
38614 <386141174@qq.com>
2019-09-04 11:19:09 +0800

    new

---
+++ src/main/java/com/hp/HptvApplication.java
@@ -0,0 +1,13 @@
+package com.hp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HptvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HptvApplication.class, args);
+    }
+
+}
