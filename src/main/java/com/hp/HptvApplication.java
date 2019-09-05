7e26a640fa33666f6a4161a9a0e3ba8c02efdc1b
38614 <386141174@qq.com>
2019-09-05 20:23:16 +0800

    new

--- src/main/java/com/hp/HptvApplication.java
+++ src/main/java/com/hp/HptvApplication.java
@@ -1,9 +1,15 @@
 package com.hp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@ServletComponentScan
+@MapperScan("com.hp.dao")
 @SpringBootApplication
+@EnableTransactionManagement
 public class HptvApplication {
 
     public static void main(String[] args) {
