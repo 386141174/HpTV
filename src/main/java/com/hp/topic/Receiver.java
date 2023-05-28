0ef12aedd60467ab7f8f4a72de2b8fceff6a057c
liusibo <386141174@qq.com>
2023-05-28 23:33:12 +0800

    create

--- src/main/java/com/hp/topic/Receiver.java
+++ src/main/java/com/hp/topic/Receiver.java
@@ -12,24 +12,24 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Component
-public class Receiver {
-
-
-
-    @Autowired
-    private OrderService orderService;
-
-    Logger logger = Logger.getLogger(this.getClass());
-
-
-    @RabbitListener(queues = "active")
-    public void send(Order order) throws Exception{
-        logger.info("接收消息"+order);
-        logger.info("接收消息时间："+new Date());
-        orderService.insertOrder(order);
-    }
-
-
-
-}
+//@Component
+//public class Receiver {
+//
+//
+//
+//    @Autowired
+//    private OrderService orderService;
+//
+//    Logger logger = Logger.getLogger(this.getClass());
+//
+//
+//    @RabbitListener(queues = "active")
+//    public void send(Order order) throws Exception{
+//        logger.info("接收消息"+order);
+//        logger.info("接收消息时间："+new Date());
+//        orderService.insertOrder(order);
+//    }
+//
+//
+//
+//}
--- src/test/java/com/hp/hptv/HptvApplicationTests.java
+++ src/test/java/com/hp/hptv/HptvApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.UUID;
+
 @RunWith(SpringRunner.class)
 @EnableAutoConfiguration
 @SpringBootTest(classes = {WebSocketConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -14,6 +16,8 @@ public class HptvApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        System.out.println(UUID.randomUUID().toString().replace("-", ""));
     }
 
 }
